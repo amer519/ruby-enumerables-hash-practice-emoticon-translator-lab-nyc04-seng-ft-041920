fbb68d980824b557b3e6ebd120fcdf995f98d824
Amer Fahmy <afahmy519@gmail.com>
2020-05-07 16:38:52 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -34,7 +34,7 @@ def get_japanese_emoticon(file_path, emoticon)
       end
     end
   end
-  retur "Sorry, that emoticon was not found"
+  return "Sorry, that emoticon was not found"
 end
 
 def get_english_meaning(file_path, emoticon)
